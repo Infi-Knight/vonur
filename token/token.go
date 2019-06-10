f9c1db99be9d0c879e49d3266620072b77e9d1a0
Ravi Soni <soniravi829@gmail.com>
2019-06-10 23:01:07 +0530

    Define some tokens

---
+++ token/token.go
@@ -0,0 +1,50 @@
+/*
+Package token implements the data structures and types defined by the Monkey
+programming language.
+*/
+package token
+
+// TokenType we will use string to store the type of our tokens.
+type TokenType string
+
+// Token a token has a type and a value.
+type Token struct {
+	Type    TokenType
+	Literal string
+}
+
+// ILLEGAL represents a token/character we don't know about.
+// EOF tells the parser where to stop.
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
+)
+
+// IDENT identifiers e.g foo, bar, add.
+// INT integer literals e.g 456
+const (
+	IDENT  = "IDENT"
+	INT    = "INT"
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
+)
+
+// Operators
+const (
+	ASSIGN = "="
+	PLUS   = "+"
+)
+
+// Delimiters
+const (
+	COMMA     = ","
+	SEMICOLON = ";"
+)
+
+// Keywords
+const (
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
+)
